c7b660f91f94031b52c72c600d4d58180e3e73c4
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-06-17 22:50:14 +0300

    added navbar and carsoul

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = { reactStrictMode: true, basePath: '' };
+
+module.exports = nextConfig;
+
+module.exports = {
+    images: {
+        domains: ['m.media-amazon.com' , "picsum.photos", "img.daisyui.com"],
+       
+    },
+}
